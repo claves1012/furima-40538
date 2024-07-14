50a5d5f306db9b61b865038b2de09c2705842406
claves1012 <claves1012@gmail.com>
2024-07-14 21:41:57 +0900

    ユーザー管理機能提出課題の修正4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
        validates :nickname, presence:true
+       validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
        validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
        validates :first_name, presence:true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
        validates :last_name_kana, presence:true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
