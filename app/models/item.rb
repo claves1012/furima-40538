0aea6d6fbcd522721402b968d2994ee76e253063
claves1012 <claves1012@gmail.com>
2024-07-18 19:55:26 +0900

    Itemモデル作成＆バリデーション記述

---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+
+  validates :image, presence:true
+  validates :name, presence:true
+  validates :description, presence:true
+  validates :category_id, presence:true
+  validates :item_status_id, presence:true
+  validates :shipping_cost_id, presence:true
+  validates :pretecture_id, presence:true
+  validates :price, presence:true
+end
