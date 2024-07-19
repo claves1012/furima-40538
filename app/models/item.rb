d05008db63a52269f561747918fa4578153591e2
claves1012 <claves1012@gmail.com>
2024-07-19 20:10:03 +0900

    プルダウンの生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,20 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :pretecture
+  belongs_to :shipping_date
+
   validates :image, presence:true
   validates :name, presence:true
   validates :description, presence:true
-  validates :category_id, presence:true
-  validates :item_status_id, presence:true
-  validates :shipping_cost_id, presence:true
-  validates :pretecture_id, presence:true
-  validates :shipping_date_id, presence:true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :pretecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence:true
 end
