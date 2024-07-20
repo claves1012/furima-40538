aa716b4e8e302fdf895d36af20653880a2bdf45d
claves1012 <claves1012@gmail.com>
2024-07-20 17:06:51 +0900

    不要に作成したモデル削除

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :item_status
-  belongs_to :shipping_cost
-  belongs_to :pretecture
-  belongs_to :shipping_date
-end
