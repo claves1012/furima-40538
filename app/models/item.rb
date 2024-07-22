15f7f73ceeac3a4b6c4c5406cd20d1e80aa77091
claves1012 <claves1012@gmail.com>
2024-07-22 21:16:14 +0900

    単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,5 +18,8 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence:true
+  validates :price,
+            presence:true,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true },
+            format: { with: /\A\d+\z/, message: "is invalid. Input half-width numbers."}
 end
