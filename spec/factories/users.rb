8f9ded6ad90b2bb6fe182f9ba50235a575e7bb7d
claves1012 <claves1012@gmail.com>
2024-08-10 18:05:04 +0900

    終了報告の修正3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     email {Faker::Internet.email}
     password {"1a" + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name {Faker::Name.last_name}
-    first_name {Faker::Name.first_name}
+    last_name { ["田中", "佐藤", "鈴木", "高橋", "渡辺"].sample }
+    first_name { ["田中", "佐藤", "鈴木", "高橋", "渡辺"].sample }
     last_name_kana { ["タナカ", "サトウ", "スズキ", "タカハシ", "ワタナベ"].sample }
     first_name_kana { ["タナカ", "サトウ", "スズキ", "タカハシ", "ワタナベ"].sample }
     birthday {Faker::Date.backward(days: 10000)}
